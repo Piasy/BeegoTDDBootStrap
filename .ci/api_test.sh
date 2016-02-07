a5da499c28c6a7e20699bbd7afebabf8ab1c3a87
Piasy <xz4215@gmail.com>
2016-02-07 15:37:53 +0800

    update script; new line

--- .ci/api_test.sh
+++ .ci/api_test.sh
@@ -1,5 +1,24 @@
-#!/bin/sh
-.ci/prepare_db.sh && \
-cd tests && go test -v .
+#!/bin/bash
+function doTest()
+{
+    pwd
+    output=`go test -v -cover . | grep -E "coverage:|FAIL:"`
+    pass=$?
+    if (( $pass != 0 )); then
+        return 1
+    fi
+    coverage=`echo $output | grep "coverage:"`
+    echo $coverage
+    fail=`echo $output | grep "FAIL:"`
+    pass=$?
+    if (( $pass == 0 )); then
+        return 1
+    fi
+    return 0
+}
 
+.ci/prepare_db.sh && \
+cd tests && doTest
+
+echo "passed: $?"
 cd .. && .ci/clean_db.sh
--- .ci/unit_test.sh
+++ .ci/unit_test.sh
@@ -1,6 +1,25 @@
-#!/bin/sh
-.ci/prepare_db.sh && \
-cd models && go test -v . && cd .. && \
-cd utils && go test -v .
+#!/bin/bash
+function doTest()
+{
+    pwd
+    output=`go test -v -cover . | grep -E "coverage:|FAIL:"`
+    pass=$?
+    if (( $pass != 0 )); then
+        return 1
+    fi
+    coverage=`echo $output | grep "coverage:"`
+    echo $coverage
+    fail=`echo $output | grep "FAIL:"`
+    pass=$?
+    if (( $pass == 0 )); then
+        return 1
+    fi
+    return 0
+}
 
+.ci/prepare_db.sh && \
+cd models && doTest && cd .. && \
+cd utils && doTest
+
+echo "passed: $?"
 cd .. && .ci/clean_db.sh
